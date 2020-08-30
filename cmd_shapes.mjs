0a0641790d29e8cab5bfc0137a5ffdabf2d6608a
Dimava <dimava2@ya.ru>
2020-08-30 17:35:53 +0300

    Update cmd_shapes.mjs

--- cmd_shapes.mjs
+++ cmd_shapes.mjs
@@ -31,7 +31,7 @@ export const cmd_any_shape = {
         let d = parseArgs(data.s, 'size')
         data.s = d.s
         let size = Math.round(d.args && +d.args[0] || 100)
-        if (!(size < 1)) size = 100;
+        if (!(size >= 1)) size = 100;
 
         let allShapesRaw = data.s.match(rg_shape)
         console.log({ s: data.s, allShapesRaw })
